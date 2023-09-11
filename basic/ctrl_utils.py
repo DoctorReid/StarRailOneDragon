2ca54a3ba5e38502292d9ed3d0566e8f48f8d1fd
DoctorReid <momojie_dyh@163.com>
2023-09-11 19:20:49 +0800

    #2 增加判断是否当前窗口的判断方法

--- basic/ctrl_utils.py
+++ basic/ctrl_utils.py
@@ -28,3 +28,26 @@ def active_win(win: Win32Window):
         win.activate()
     else:
         raise pyautogui.PyAutoGUIException
+
+
+def is_active_win(win: Win32Window) -> bool:
+    """
+    判断窗口是否最前激活
+    :param win: 窗口
+    :return: 是否最前激活 空窗口返回False
+    """
+    return win.isActive if win is not None else False
+
+
+def is_active_win_by_name(window_name: str):
+    """
+    根据判断窗口是否最前激活
+    :param window_name: 窗口名称
+    :return: 如果窗口不存在 返回False 否则返回是否激活
+    """
+    try:
+        win = get_win_by_name(window_name)
+        return is_active_win(win)
+    except pyautogui.PyAutoGUIException:
+        return False
+
