aa25ce013bff8dc902d237e9635991367674495b
DoctorReid <momojie_dyh@163.com>
2023-10-18 22:11:59 +0800

    #48 雅利洛 - 旧武器试验场L2 加入地图

--- src/sr/constants/map.py
+++ src/sr/constants/map.py
@@ -90,12 +90,13 @@ P02_R04 = Region("twjq", "铁卫禁区", P02_YYL)
 P02_R05 = Region("cxhl", "残响回廊", P02_YYL)
 P02_R06 = Region("ydl", "永冬岭", P02_YYL)
 P02_R07 = Region("zwzz", "造物之柱", P02_YYL)
+P02_R08_L2 = Region("jwqsyc", "旧武器试验场", P02_YYL, level=2)
 P02_R09_MDZ = Region("mdz", "铆钉镇", P02_YYL)
 P02_R10 = Region("dkq", "大矿区", P02_YYL)
 
 PLANET_2_REGION = {
     P01_KZJ.id: [P01_R01_ZKCD, P01_R02_JZCD, P01_R03_SRCD_L1, P01_R03_SRCD_L2, P01_R03_SRCD_B1, P01_R04_ZYCD_L1, P01_R04_ZYCD_L2],
-    P02_YYL.id: [P02_R01_L1, P02_R01_B1, P02_R02, P02_R03, P02_R04, P02_R05, P02_R06, P02_R07, P02_R09_MDZ, P02_R10],
+    P02_YYL.id: [P02_R01_L1, P02_R01_B1, P02_R02, P02_R03, P02_R04, P02_R05, P02_R06, P02_R07, P02_R08_L2, P02_R09_MDZ, P02_R10],
     P03_XZLF.id: []
 }
 
