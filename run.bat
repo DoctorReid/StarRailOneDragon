8797701e499bc45e7156e2ce3721eed787d24416
DoctorReid <momojie_dyh@163.com>
2023-12-30 17:57:08 +0800

    #126 启动脚本简化

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ cd /D %~dp0
 
 echo 如无反应 可切换使用管理员权限运行脚本
 
-set local_python=%~dp0.env\venv\Scripts\python.exe
+set local_python=.env\venv\Scripts\python.exe
 set PYTHONPATH=%~dp0/src
 
 !local_python! src/gui/app.py
