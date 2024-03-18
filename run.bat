dd47e1a5300d2f26ef2a548fe7424e4fe8bba646
DoctorReid <momojie_dyh@163.com>
2024-03-18 20:33:49 +0800

    #126 回滚

--- run.bat
+++ run.bat
@@ -5,10 +5,8 @@ cd /D %~dp0
 
 echo 如无反应 可切换使用管理员权限运行脚本
 
-REM set local_python=%~dp0.env\venv\Scripts\python.exe
+set local_python=.env\venv\Scripts\python.exe
 set PYTHONPATH=%~dp0/src
-set DEBUG=1
 
-REM !local_python! src/gui/app.py
-python src/gui/app.py
+!local_python! src/gui/app.py
 pause
