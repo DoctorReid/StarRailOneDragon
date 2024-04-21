2e39304ebf937d18f30311194d93964b5b120e80
DoctorReid <momojie_dyh@163.com>
2024-04-21 14:00:07 +0800

    #252 减少必要的刷新

--- src/gui/one_stop_view.py
+++ src/gui/one_stop_view.py
@@ -337,8 +337,6 @@ class OneStopView(ft.Row, SrBasicView):
         self.app_list.refresh_app_order()
         self._set_schedule()
         scheduler.every_second(self._update_app_list_status, tag='_update_app_list_status', seconds=10)
-        scheduler.every_second(self._update_character_status, tag='_update_character_status', seconds=10)
-        scheduler.every_second(self.app_list.refresh_app_order, tag='refresh_app_order', seconds=10)
         self.sr_ctx.register_status_changed_handler(self,
                                                     self._after_start,
                                                     self._after_pause,
@@ -430,6 +428,9 @@ class OneStopView(ft.Row, SrBasicView):
     def _after_pause(self):
         self._update_status_component()
 
+        self._update_app_list_status()
+        self._update_running_app_name()
+
     def _after_resume(self):
         self._update_status_component()
 
@@ -438,6 +439,7 @@ class OneStopView(ft.Row, SrBasicView):
         self.app_list.set_disabled(False)
         scheduler.cancel_with_tag('_update_running_app_name')
         self.running_app = None
+        self._update_app_list_status()
         self._update_running_app_name()
 
         if self.after_done_dropdown.value == 'shutdown':
