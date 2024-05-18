3d7c9e8918cb6f7293f0cb8c985ca4009538e74d
DoctorReid <momojie_dyh@163.com>
2024-05-18 16:12:41 +0800

    #299 增加对活动委托的兼容

--- src/sr/app/assignments/assignments_app.py
+++ src/sr/app/assignments/assignments_app.py
@@ -3,14 +3,13 @@ from typing import List, ClassVar
 from cv2.typing import MatLike
 
 from basic.i18_utils import gt
-from basic.img import MatchResult
+from basic.img import MatchResult, cv2_utils, MatchResultList
 from sr.app.application_base import Application
 from sr.const import phone_menu_const
 from sr.context import Context
 from sr.image.sceenshot import phone_menu
 from sr.operation import Operation, StateOperationNode, StateOperationEdge, OperationOneRoundResult
 from sr.operation.common.back_to_normal_world_plus import BackToNormalWorldPlus
-from sr.operation.unit.claim_assignment import ClaimAssignment
 from sr.operation.unit.menu.open_phone_menu import OpenPhoneMenu
 from sr.screen_area.screen_phone_menu import ScreenPhoneMenu
 
@@ -19,6 +18,8 @@ class AssignmentsApp(Application):
 
     STATUS_WITH_ALERT: ClassVar[str] = '委托红点'
     STATUS_NO_ALERT: ClassVar[str] = '无委托红点'
+    STATUS_NO_ALL_CLAIM: ClassVar[str] = '无一键领取'
+    STATUS_NO_CLAIM: ClassVar[str] = '无领取'
 
     def __init__(self, ctx: Context):
         edges: List[StateOperationEdge] = []
@@ -30,15 +31,33 @@ class AssignmentsApp(Application):
         click = StateOperationNode('点击委托', self._click_assignment)
         edges.append(StateOperationEdge(menu, click))
 
-        claim = StateOperationNode('领取委托奖励', self._claim)
-        edges.append(StateOperationEdge(click, claim, status=AssignmentsApp.STATUS_WITH_ALERT))
+        claim_all = StateOperationNode('一键领取', self._claim_all)
+        edges.append(StateOperationEdge(click, claim_all, status=AssignmentsApp.STATUS_WITH_ALERT))
 
         send = StateOperationNode('再次派遣', self._send)
-        edges.append(StateOperationEdge(claim, send))
+        edges.append(StateOperationEdge(claim_all, send))
+
+        # 可能有活动 导致没有一键领取
+        claim = StateOperationNode('领取', self._claim)
+        edges.append(StateOperationEdge(claim_all, claim, status=AssignmentsApp.STATUS_NO_ALL_CLAIM))
+
+        # 领取后出现空白
+        click_empty = StateOperationNode('点击空白', self._click_empty)
+        edges.append(StateOperationEdge(claim, click_empty))
+        # 空白后继续领取
+        edges.append(StateOperationEdge(click_empty, claim))
+
+        # 活动的领取完了 找红点到普通委托
+        click_alert = StateOperationNode('点击红点', self._click_alert_category)
+        edges.append(StateOperationEdge(claim, click_alert, status=AssignmentsApp.STATUS_NO_CLAIM))
+
+        # 红点之后 回到一键委托
+        edges.append(StateOperationEdge(click_alert, claim_all))
 
         back = StateOperationNode('完成后返回大世界', op=BackToNormalWorldPlus(ctx))
         edges.append(StateOperationEdge(send, back))
         edges.append(StateOperationEdge(click, back, status=AssignmentsApp.STATUS_NO_ALERT))
+        edges.append(StateOperationEdge(click_alert, back, success=False))  # 没有红点也返回
 
         super().__init__(ctx, op_name=gt('委托', 'ui'),
                          run_record=ctx.assignments_run_record,
@@ -55,13 +74,16 @@ class AssignmentsApp(Application):
             return Operation.round_success(AssignmentsApp.STATUS_NO_ALERT)
         else:
             self.ctx.controller.click(result.center)
-            return Operation.round_success(AssignmentsApp.STATUS_WITH_ALERT, wait=1)
+            return Operation.round_success(AssignmentsApp.STATUS_WITH_ALERT, wait=2)
 
-    def _claim(self) -> OperationOneRoundResult:
+    def _claim_all(self) -> OperationOneRoundResult:
         screen = self.screenshot()
-        area = ScreenPhoneMenu.ASSIGNMENTS_CLAIM.value
-        if self.find_and_click_area(area, screen) == Operation.OCR_CLICK_SUCCESS:
+        area = ScreenPhoneMenu.ASSIGNMENTS_CLAIM_ALL.value
+        click = self.find_and_click_area(area, screen)
+        if click == Operation.OCR_CLICK_SUCCESS:
             return Operation.round_success()
+        elif click == Operation.OCR_CLICK_NOT_FOUND:
+            return Operation.round_success(status=AssignmentsApp.STATUS_NO_ALL_CLAIM)
         else:
             return Operation.round_retry(status='点击%s失败' % area.status, wait=1)
 
@@ -72,6 +94,38 @@ class AssignmentsApp(Application):
             return Operation.round_success()
         else:
             return Operation.round_retry(status='点击%s失败' % area.status, wait=1)
+
+    def _claim(self) -> OperationOneRoundResult:
+        screen = self.screenshot()
+        area = ScreenPhoneMenu.ASSIGNMENTS_CLAIM.value
+        click = self.find_and_click_area(area, screen)
+        if click == Operation.OCR_CLICK_SUCCESS:
+            return Operation.round_success(wait=1)
+        elif click == Operation.OCR_CLICK_NOT_FOUND:
+            return Operation.round_success(status=AssignmentsApp.STATUS_NO_CLAIM)
+        else:
+            return Operation.round_retry(status='点击%s失败' % area.status, wait=1)
+
+    def _click_empty(self):
+        screen = self.screenshot()
+        area = ScreenPhoneMenu.ASSIGNMENTS_CLICK_EMPTY.value
+        click = self.find_and_click_area(area, screen)
+        if click == Operation.OCR_CLICK_SUCCESS:
+            return Operation.round_success(wait=1)
+        else:
+            return Operation.round_retry(status='点击%s失败' % area.status, wait=1)
+
+    def _click_alert_category(self):
+        screen = self.screenshot()
+        area = ScreenPhoneMenu.ASSIGNMENTS_CATEGORY_RECT.value
+        category_part = cv2_utils.crop_image_only(screen, area.rect)
+        result_list: MatchResultList = self.ctx.im.match_template(category_part, 'ui_alert')
+
+        if len(result_list) > 0:  # 有红点
+            self.ctx.controller.click(area.rect.left_top + result_list.max.center)
+            return Operation.round_success(wait=1)
+
+        return Operation.round_retry('无红点')
 
     @property
     def current_execution_desc(self) -> str:
--- src/sr/screen_area/screen_phone_menu.py
+++ src/sr/screen_area/screen_phone_menu.py
@@ -14,5 +14,8 @@ class ScreenPhoneMenu(Enum):
 
     POWER_BTN = ScreenArea(pc_rect=Rect(1834, 951, 1897, 1008), status='返回登陆')
 
-    ASSIGNMENTS_CLAIM = ScreenArea(pc_rect=Rect(370, 886, 573, 933), text='一键领取')  # 委托
+    ASSIGNMENTS_CATEGORY_RECT = ScreenArea(pc_rect=Rect(320, 190, 1200, 280))  # 委托 上方类目
+    ASSIGNMENTS_CLAIM_ALL = ScreenArea(pc_rect=Rect(370, 886, 573, 933), text='一键领取')  # 委托
+    ASSIGNMENTS_CLAIM = ScreenArea(pc_rect=Rect(1400, 880, 1530, 920), text='领取')  # 委托
     ASSIGNMENTS_SEND_AGAIN = ScreenArea(pc_rect=Rect(1037, 927, 1374, 981), text='再次派遣')  # 委托
+    ASSIGNMENTS_CLICK_EMPTY = ScreenArea(pc_rect=Rect(857, 928, 1068, 963), text='点击空白区域继续')  # 委托
