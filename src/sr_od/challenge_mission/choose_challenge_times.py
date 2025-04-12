941cb455989dd1681cc6185ae10e1791c246ebac
DoctorReid <doctorreid2024@outlook.com>
2025-04-12 20:47:05 +0800

    #458 修复拟造花萼

--- src/sr_od/challenge_mission/choose_challenge_times.py
+++ src/sr_od/challenge_mission/choose_challenge_times.py
@@ -14,8 +14,6 @@ from sr_od.operations.sr_operation import SrOperation
 
 class ChooseChallengeTimes(SrOperation):
 
-    CURRENT_TIMES_RECT: ClassVar[Rect] = Rect(1580, 850, 1620, 890)
-
     def __init__(self, ctx: SrContext, total_times: int):
         """
         在挑战副本的选择难度页面
@@ -43,16 +41,19 @@ class ChooseChallengeTimes(SrOperation):
             if self._click_minus(screen, self.total_times - current):
                 return self.round_success()
 
+        return self.round_retry('未能判断当前选择次数', wait=1)
+
     def _get_current_times(self, screen: MatLike) -> int:
         """
         判断当前选择的次数
         :param screen: 屏幕截图
         :return: 当前选择次数
         """
-        part = cv2_utils.crop_image_only(screen, ChooseChallengeTimes.CURRENT_TIMES_RECT)
+        area = self.ctx.screen_loader.get_area('拟造花萼', '文本-挑战次数')
+        part = cv2_utils.crop_image_only(screen, area.rect)
         # cv2_utils.show_image(part, win_name='_get_current_times')
         ocr_result = self.ctx.ocr.run_ocr_single_line(part, strict_one_line=True)
-        return str_utils.get_positive_digits(ocr_result)
+        return str_utils.get_positive_digits(ocr_result, err=0)
 
     def _click_plus(self, screen: MatLike, click_times: int) -> bool:
         """
