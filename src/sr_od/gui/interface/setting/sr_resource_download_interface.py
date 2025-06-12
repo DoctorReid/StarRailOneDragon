f9cb3e5968cef25623456ebe6587a16f9d86809a
Qian23333 <119576779+Qian23333@users.noreply.github.com>
2025-06-13 02:38:35 +0800

    同步模型下载卡片

--- src/sr_od/gui/interface/setting/sr_resource_download_interface.py
+++ src/sr_od/gui/interface/setting/sr_resource_download_interface.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QWidget
 from qfluentwidgets import SettingCardGroup, FluentIcon
 
+from one_dragon.base.web.common_downloader import CommonDownloaderParam
 from one_dragon_qt.view.setting.resource_download_interface import ResourceDownloadInterface
 from one_dragon_qt.widgets.setting_card.onnx_model_download_card import OnnxModelDownloadCard
 from sr_od.config.model_config import get_world_patrol_opts, get_sim_uni_opts
@@ -37,7 +38,7 @@ class SrResourceDownloadInterface(ResourceDownloadInterface):
     def init_world_patrol_opts(self) -> None:
         self.world_patrol_model_opt.blockSignals(True)
         self.world_patrol_model_opt.set_options_by_list(get_world_patrol_opts())
-        self.world_patrol_model_opt.setValue(self.ctx.model_config.world_patrol)
+        self.world_patrol_model_opt.set_value_by_save_file_name(f'{self.ctx.model_config.world_patrol}.zip')
         self.world_patrol_model_opt.gpu_opt.setChecked(self.ctx.model_config.world_patrol_gpu)
         self.world_patrol_model_opt.check_and_update_display()
         self.world_patrol_model_opt.blockSignals(False)
@@ -45,20 +46,20 @@ class SrResourceDownloadInterface(ResourceDownloadInterface):
     def init_sim_uni_model_opts(self) -> None:
         self.sim_uni_model_opt.blockSignals(True)
         self.sim_uni_model_opt.set_options_by_list(get_sim_uni_opts())
-        self.sim_uni_model_opt.setValue(self.ctx.model_config.sim_uni)
+        self.sim_uni_model_opt.set_value_by_save_file_name(f'{self.ctx.model_config.sim_uni}.zip')
         self.sim_uni_model_opt.gpu_opt.setChecked(self.ctx.model_config.sim_uni_gpu)
         self.sim_uni_model_opt.check_and_update_display()
         self.sim_uni_model_opt.blockSignals(False)
 
-    def on_world_patrol_model_changed(self, index: int, value: str) -> None:
-        self.ctx.model_config.world_patrol = value
+    def on_world_patrol_model_changed(self, index: int, value: CommonDownloaderParam) -> None:
+        self.ctx.model_config.world_patrol = value.save_file_name[:-4]
         self.world_patrol_model_opt.check_and_update_display()
 
     def on_world_patrol_gpu_changed(self, value: bool) -> None:
         self.ctx.model_config.world_patrol_gpu = value
 
-    def _on_sim_uni_model_changed(self, index: int, value: str) -> None:
-        self.ctx.model_config.sim_uni = value
+    def _on_sim_uni_model_changed(self, index: int, value: CommonDownloaderParam) -> None:
+        self.ctx.model_config.sim_uni = value.save_file_name[:-4]
         self.sim_uni_model_opt.check_and_update_display()
 
     def on_sim_uni_gpu_changed(self, value: bool) -> None:
