182bfa4ee21bb72da00bae197e9c4198f05ce4ef
Qian23333 <119576779+Qian23333@users.noreply.github.com>
2025-06-14 23:38:00 +0800

    修复一条龙启动路径

--- src/sr_od/gui/sr_launcher.py
+++ src/sr_od/gui/sr_launcher.py
@@ -12,7 +12,7 @@ class SrLauncher(ExeLauncher):
         ExeLauncher.__init__(self, "星穹铁道 一条龙 启动器", __version__)
 
     def run_onedragon_mode(self, launch_args) -> None:
-        python_launcher.run_python(["sr_od", "application", "sr_application_launcher.py"], no_windows=False, args=launch_args)
+        python_launcher.run_python(["sr_od", "app", "sr_application_launcher.py"], no_windows=False, args=launch_args)
 
     def run_gui_mode(self) -> None:
         python_launcher.run_python(["sr_od", "gui", "sr_full_app.py"], no_windows=True)
